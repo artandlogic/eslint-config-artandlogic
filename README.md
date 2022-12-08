1fe2d70a4c7e867bb7792df48073016d31630975
Paul Hendry <phendry@artandlogic.com>
2022-12-08 15:30:27 -0800

    Initial setup based on eslint:recommended and Prettier

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# eslint-config-artandlogic
-An ESLint Shareable Config for Art+Logic coding style
+# `eslint-config-artandlogic`
+
+An [ESLint](https://eslint.org/)
+[Shareable Config](https://eslint.org/docs/latest/developer-guide/shareable-configs)
+for Art+Logic coding style
---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+/**
+ * @file Configures Prettier for automated code formatting.
+ *       Prettier configuration reference: https://prettier.io/docs/en/configuration.html
+ */
+
+module.exports = {
+    // Always wrap prose such as Markdown to the print width
+    proseWrap: "always",
+    // Set tab width to the A+L-preferred 4
+    tabWidth: 4,
+};
